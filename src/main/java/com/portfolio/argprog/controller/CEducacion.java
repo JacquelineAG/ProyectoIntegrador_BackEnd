7aa73ddaaad1424d09fd76b8f8624d30b548d480
Jacqueline <107328708+JacquelineAG@users.noreply.github.com>
2022-08-24 00:05:02 -0300

    BackEnd PreAuthorize

--- src/main/java/com/portfolio/argprog/controller/CEducacion.java
+++ src/main/java/com/portfolio/argprog/controller/CEducacion.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,6 +42,7 @@ public class CEducacion {
         return new ResponseEntity(educacion, HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         if(!sEducacion.existsById(id)){
@@ -50,6 +52,7 @@ public class CEducacion {
         return new ResponseEntity(new Mensaje("Educacion eliminada correctamente."), HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoEducacion dtoeducacion){
         if(StringUtils.isBlank(dtoeducacion.getNombreE())){
@@ -67,6 +70,7 @@ public class CEducacion {
                 
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoEducacion dtoeducacion){
         if(!sEducacion.existsById(id)){
--- src/main/java/com/portfolio/argprog/controller/CIdioma.java
+++ src/main/java/com/portfolio/argprog/controller/CIdioma.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,6 +42,7 @@ public class CIdioma {
         return new ResponseEntity(idioma, HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         if(!sIdioma.existsById(id)){
@@ -50,6 +52,7 @@ public class CIdioma {
         return new ResponseEntity(new Mensaje("Idioma eliminado correctamente."), HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoIdioma dtoidioma){
         if(StringUtils.isBlank(dtoidioma.getNombreE())){
@@ -67,6 +70,7 @@ public class CIdioma {
                 
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoIdioma dtoidioma){
         if(!sIdioma.existsById(id)){
