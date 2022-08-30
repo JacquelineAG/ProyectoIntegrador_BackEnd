484d0fd5ac3975f11c1d47e5bf7c4635709d351c
Jacqueline <107328708+JacquelineAG@users.noreply.github.com>
2022-08-29 23:30:54 -0300

    Back edit

--- src/main/java/com/portfolio/argprog/controller/CInfo.java
+++ src/main/java/com/portfolio/argprog/controller/CInfo.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/acercade")
+@RequestMapping("/info")
 @CrossOrigin(origins = "https://argprogfrontend.web.app")
 public class CInfo {
     @Autowired
